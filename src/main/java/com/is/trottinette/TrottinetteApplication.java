707a5c1d139a565c15fca98b49dd875d86713e43
Ayman <ammariayman95@gmail.com>
2020-09-08 20:28:27 +0200

    Project initiation

---
+++ src/main/java/com/is/trottinette/TrottinetteApplication.java
@@ -0,0 +1,13 @@
+package com.is.trottinette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrottinetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrottinetteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/is/trottinette/TrottinetteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is.trottinette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrottinetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
